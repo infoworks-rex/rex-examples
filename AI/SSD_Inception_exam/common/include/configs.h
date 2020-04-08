4be58526c4a4755ffd97d13efff3750fbba53aed
hwangjy <hwangjy@info-works.co.kr>
2020-04-08 01:06:17 +0000

    AI

--- AI/SSD_Inception_exam/common/include/configs.h
+++ AI/SSD_Inception_exam/common/include/configs.h
@@ -9,7 +9,8 @@
 #define DISP_BPP	4
 #define DISP_FMT	RK_FORMAT_BGRA_8888
 
-#define CAM_W	 	640
+#define CAM_CH		3
+#define CAM_W		640
 #define CAM_H		480
 #define CAM_SIZE	CAM_W * CAM_H
 #define CAM_FMT 	V4L2_PIX_FMT_YUV420
--- AI/SSD_Inception_exam/src/main.cc
+++ AI/SSD_Inception_exam/src/main.cc
@@ -21,17 +21,23 @@
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 
+//common/include
+#include "rknn_helper.h"
 #include "drm_module.h"
 #include "camera_helper.h"
 #include "rga_helper.h"
-
 #include "configs.h"
-
+#include "rknn_helper.h"
+//inc
+//extern "C"{
+#include "ssd.h"
+//};
 uint8_t *camera_buf;
 uint8_t *display_buf;
 uint8_t *buf;
 uint8_t *out;
 
+ssd_ctx ctx; //for ssd data save
 
 pthread_cond_t thread_cond = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex_lock = PTHREAD_MUTEX_INITIALIZER;
@@ -40,6 +46,50 @@ int drm_fd, cam_fd;
 drm_dev_t *modeset_list = NULL;
 const char* card = CARD_DEV;
 
+pthread_key_t a_key;
+
+#define VIDEO_PACKET_SIZE CAM_H*CAM_W*CAM_CH
+#define MODEL_NAME	"/usr/share/rknn_demo/ssd_inception_v2.rknn"
+#define PRIOR_BOX 	"/usr/share/rknn_demo/box_priors.txt"
+#define LABELS		"/usr/share/rknn_demo/coco_labels_list.txt"
+
+
+void *ssd_loop(void *ptr)
+{	
+
+
+	void* dst_v = NULL;
+	dst_v = malloc(VIDEO_PACKET_SIZE);
+	
+	int ret;
+	c_RkRgaInit();
+	
+	rga_info_t r_src;	
+	rga_info_t r_dst;	
+	
+	memset(&r_src, 0, sizeof(rga_info_t));
+	r_src.fd = -1;
+	r_src.mmuFlag = 1;
+	r_src.virAddr = camera_buf;
+	memset(&r_dst, 0, sizeof(rga_info_t));
+	r_dst.fd = -1;
+	r_dst.mmuFlag = 1;
+	r_dst.virAddr = dst_v;
+	r_src.rotation = 0;
+
+	ssd_init(MODEL_NAME,PRIOR_BOX ,LABELS , &ctx);
+	printf("init rknn complete \n");	
+	while(1){
+		rga_set_rect(&r_src.rect, 0, 0, CAM_W, CAM_H, CAM_W, CAM_H, RK_FORMAT_YCrCb_420_P);
+		rga_set_rect(&r_dst.rect, 0, 0, 300, 300, 300, 300, RK_FORMAT_RGB_888 );
+		ret = c_RkRgaBlit(&r_src, &r_dst, NULL);
+
+		ssd_run(&ctx,(uint8_t*)dst_v,300,300,270000);
+		
+		//printf(" detections count %d , %d \n ",ctx.detections.count, ctx.detections.objects[0].rect.left);
+	}
+
+} 
 
 
 void *camera_loop(void *ptr)
@@ -51,15 +101,14 @@ void *camera_loop(void *ptr)
 	ret = camera_streamon(cam_fd, V4L2_BUF_LEN);
 
     while (1) {
-        pthread_mutex_lock(&mutex_lock);
-
+	    pthread_mutex_lock(&mutex_lock);
         ret = camera_get_frame_helper(cam_fd, &camera_buf, &size);
 
         status = 1;
-        pthread_cond_signal(&thread_cond);
         pthread_mutex_unlock(&mutex_lock);
     }   
 }
+
 
 void *display_loop(void *ptr)
 {
@@ -72,28 +121,35 @@ void *display_loop(void *ptr)
     struct timeval start, end;
     double diffTime;
 
-    src.data        = camera_buf;
+    src.data    = camera_buf;
     src.width   = CAM_H;
     src.height  = CAM_W;
     src.format  = RK_FORMAT_YCrCb_420_P;
     src.direction = 0;
 
-    dst.data        = modeset_list->map;
+    dst.data    = modeset_list->map;
     dst.width   = DISP_H;
     dst.height  = DISP_W;
     dst.format  = RK_FORMAT_RGBA_8888;
-    
+   
 	//init drm
 	
 	printf("Display Thread Set");
     while (1) {
+
 		gettimeofday(&start, NULL);
+
+
         rga_transform(&src, &dst);
+
+//	    pthread_mutex_lock(&mutex_lock);
+		draw_boundingbox(ctx, dst);
+	
+//      pthread_mutex_unlock(&mutex_lock);
 
         gettimeofday(&end, NULL);
         diffTime = (end.tv_sec - start.tv_sec) * 1000.0;      // sec to ms
         diffTime += (end.tv_usec - start.tv_usec) / 1000.0;   // us to ms
-        //printf("Display Time : %f\n", diffTime);
     }
 }
 
@@ -103,6 +159,7 @@ int main(int argc, char **argv)
    	//thread setting
 	int ret;
 	pthread_t camera_thread, display_thread;
+	pthread_t ssd_thread;
 
 	camera_buf = (uint8_t *)malloc(CAM_W * CAM_H * CAM_BPP);
 	display_buf = (uint8_t *)malloc(DISP_W * DISP_H * DISP_BPP);
@@ -115,27 +172,35 @@ int main(int argc, char **argv)
 	printf("drm_init _finish \n");
 	rga_init_helper();
 	printf("rga_init _finish \n");
+
+	ret = pthread_setspecific(a_key, (uint8_t *)camera_buf);
    
 	ret = pthread_create(&camera_thread, NULL, camera_loop, NULL);
     if (ret < 0) {
-        perror("pthread create error: ");
+        perror("camera pthread create error: ");
+        exit(EXIT_FAILURE);
+    }
+
+	ret = pthread_create(&ssd_thread, NULL, ssd_loop, NULL);
+    if (ret < 0) {
+        perror("ssd_run pthread create error: ");
         exit(EXIT_FAILURE);
     }
 
     ret = pthread_create(&display_thread, NULL, display_loop, NULL);
     if (ret < 0) {
-        perror("pthread create error: ");
+        perror("display pthread create error: ");
         exit(EXIT_FAILURE);
     }
 
 	printf("%s main pthread start \n",__func__);    
     int status;
     ret = pthread_join(camera_thread, NULL);
-//  ret = pthread_join(display_thread, NULL);
+	ret = pthread_join(display_thread, NULL);
+	ret = pthread_join(ssd_thread, NULL);
   
     return 0;
 
     
 }
-
 
